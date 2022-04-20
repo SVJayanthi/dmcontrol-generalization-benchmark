LUSR fix scaling 255

src/algorithms/lusr.py
@@ -22,45 +22,49 @@ class LUSR(SAC):
 		self.svea_beta = args.svea_beta
 
 		self.lusr = m.LUSR(self.shared_cnn, self.head_cnn, obs_shape, args.num_shared_layers, args.num_filters).cuda()
-		self.lusr_optimizer = torch.optim.Adam(self.lusr.parameters(), lr=args.actor_lr)
+		self.lusr_optimizer = torch.optim.Adam(self.lusr.parameters(), lr=1e-2)
 
 	def vae_loss(self, x, mu, logsigma, recon_x, beta=1):
 		recon_loss = F.mse_loss(x, recon_x, reduction='mean')
-		kl_loss = -0.5 * torch.sum(1 + logsigma - mu.pow(2) - logsigma.exp())
-		kl_loss = kl_loss / torch.numel(x)
-		return recon_loss + kl_loss * beta
+		# kl_loss = -0.5 * torch.sum(1 + logsigma - mu.pow(2) - logsigma.exp())
+		# kl_loss = kl_loss / torch.numel(x)
+		return recon_loss # + kl_loss * beta
 
 	def forward_loss(self, x, beta):
-		mu, logsigma, classcode = self.lusr.encoder(x)
-		contentcode = reparameterize(mu, logsigma)
-		shuffled_classcode = classcode[torch.randperm(classcode.shape[0])]
+		mu, classcode = self.lusr.encoder(x)
+		# contentcode = reparameterize(mu, logsigma)
+		# shuffled_classcode = classcode[torch.randperm(classcode.shape[0])]
 
-		latentcode1 = torch.cat([contentcode, shuffled_classcode], dim=1)
-		latentcode2 = torch.cat([contentcode, classcode], dim=1)
+		# latentcode1 = torch.cat([contentcode, shuffled_classcode], dim=1)
+		latentcode2 = torch.cat([mu, classcode], dim=1)
 
-		recon_x1 = self.lusr.decoder(latentcode1)
-		recon_x2 = self.lusr.decoder(latentcode2)
+		# recon_x1 = self.lusr.decoder(latentcode1)
+		recon_imgs = self.lusr.decoder(latentcode2)*255
 
-		return self.vae_loss(x, mu, logsigma, recon_x1, beta) + self.vae_loss(x, mu, logsigma, recon_x2, beta)
+		# print(x[0])
+		# print(recon_imgs[0])
+
+		return F.l1_loss(x, recon_imgs)
+	#self.vae_loss(x, mu, logsigma, recon_x1, beta) + self.vae_loss(x, mu, logsigma, recon_x2, beta)
 
 	def backward_loss(self, x, device=None):
-		mu, logsigma, classcode = self.lusr.encoder(x)
+		mu, classcode = self.lusr.encoder(x)
 		shuffled_classcode = classcode[torch.randperm(classcode.shape[0])]
 		randcontent = torch.randn_like(mu) #.to(device)
 
 		latentcode1 = torch.cat([randcontent, classcode], dim=1)
 		latentcode2 = torch.cat([randcontent, shuffled_classcode], dim=1)
 
-		recon_imgs1 = self.lusr.decoder(latentcode1).detach()
-		recon_imgs2 = self.lusr.decoder(latentcode2).detach()
+		recon_imgs1 = self.lusr.decoder(latentcode1).detach()*255
+		recon_imgs2 = self.lusr.decoder(latentcode2).detach()*255
 
-		cycle_mu1, cycle_logsigma1, cycle_classcode1 = self.lusr.encoder(recon_imgs1)
-		cycle_mu2, cycle_logsigma2, cycle_classcode2 = self.lusr.encoder(recon_imgs2)
+		cycle_mu1, cycle_classcode1 = self.lusr.encoder(recon_imgs1)
+		cycle_mu2, cycle_classcode2 = self.lusr.encoder(recon_imgs2)
 
-		cycle_contentcode1 = reparameterize(cycle_mu1, cycle_logsigma1)
-		cycle_contentcode2 = reparameterize(cycle_mu2, cycle_logsigma2)
+		# cycle_contentcode1 = reparameterize(cycle_mu1, cycle_logsigma1)
+		# cycle_contentcode2 = reparameterize(cycle_mu2, cycle_logsigma2)
 
-		bloss = F.l1_loss(cycle_contentcode1, cycle_contentcode2)
+		bloss = F.l1_loss(cycle_mu1, cycle_mu2)
 		return bloss
 
 	def lusr_loss(self, x, beta):
@@ -93,23 +97,23 @@ class LUSR(SAC):
 		imgs = obs.to('cuda', non_blocking=True)
 		# for _ in range(augments):
 		# 	imgs.append(augmentations.random_conv(obs.clone()))
-		# imgs = np.array(imgs)
 		self.lusr_optimizer.zero_grad()
 		floss = self.forward_loss(imgs, 10)
 		floss = floss / imgs.shape[0]
-		print("FORWARD LOSS: ", floss)
+		print("FORWARD LOSS: ", floss.item())
 
 		imgs = utils.cat(imgs, augmentations.random_conv(imgs.clone()))
 		# backward circle
 		# imgs = imgs.reshape(-1, *imgs.shape[2:])
 		bloss = self.backward_loss(imgs)
-		print("BACKWARD LOSS: ", floss)
+		print("BACKWARD LOSS: ", bloss.item())
 		# floss, bloss = self.lusr_loss(imgs, 10)
 		# floss = floss / imgs.shape[0]
 
 		(floss + bloss).backward()
+		# floss.backward()
 		self.lusr_optimizer.step()
-		print("STEP")
+		# print("STEP")
 		return floss, bloss
 
 	def update_critic(self, obs, action, reward, next_obs, not_done, L=None, step=None):
@@ -145,12 +149,30 @@ class LUSR(SAC):
 		critic_loss.backward()
 		self.critic_optimizer.step()
 
+	def update_init(self, replay_buffer, L, step):
+		obs, action, reward, next_obs, not_done = replay_buffer.sample_drq()
+
+		self.update_critic(obs, action, reward, next_obs, not_done, L, step)
+
+		# floss, bloss = self.update_lusr(obs[:4])
+		# if L is not None:
+		# 	L.log('train/forward_loss', floss, step)
+		# 	L.log('train/backward_loss', bloss, step)
+
+		if step % self.actor_update_freq == 0:
+			self.update_actor_and_alpha(obs, L, step)
+
+		if step % self.critic_target_update_freq == 0:
+			self.soft_update_critic_target()
+
+		return
+
 	def update(self, replay_buffer, L, step):
 		obs, action, reward, next_obs, not_done = replay_buffer.sample_drq()
 
 		self.update_critic(obs, action, reward, next_obs, not_done, L, step)
 
-		floss, bloss = self.update_lusr(obs)
+		floss, bloss = self.update_lusr(obs[:16])
 		if L is not None:
 			L.log('train/forward_loss', floss, step)
 			L.log('train/backward_loss', bloss, step)
@@ -160,3 +182,5 @@ class LUSR(SAC):
 
 		if step % self.critic_target_update_freq == 0:
 			self.soft_update_critic_target()
+
+		return floss, bloss

src/utils.py
@@ -136,8 +136,8 @@ class ReplayBuffer(object):
 		obses, next_obses = [], []
 		for i in idxs:
 			obs, next_obs = self._obses[i]
-			obses.append(np.array(obs, copy=False)/255)
-			next_obses.append(np.array(next_obs, copy=False)/255)
+			obses.append(np.array(obs, copy=False))
+			next_obses.append(np.array(next_obs, copy=False))
 		return np.array(obses), np.array(next_obses)
 
 	def sample_soda(self, n=None):